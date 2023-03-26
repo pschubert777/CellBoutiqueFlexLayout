d79eea7292d97627c8fea0ca2d5b814dba955530
peterschubert <pschub99@gmail.com>
2023-03-26 15:14:32 -0400

    initial commit

---
+++ CellBoutiqueFlexLayout/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CellBoutiqueFlexLayout;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ CellBoutiqueFlexLayout/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CellBoutiqueFlexLayout;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
